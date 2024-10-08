8014a2c1926d52c612b22347b5bfc27865261176
domjgiordano <dominick.giordano@payzer.com>
2024-10-08 17:38:22 -0400

    add secret

--- terraform/secrets_manager.tf
+++ terraform/secrets_manager.tf
@@ -21,3 +21,14 @@ data "aws_secretsmanager_secret" "spotify_creds" {
 data "aws_secretsmanager_secret_version" "spotify_creds" {
   secret_id = data.aws_secretsmanager_secret.spotify_creds.id
 }
+
+resource "aws_secretsmanager_secret" "api_secret_key" {
+  name = "api_secret_key"
+  description = "API Secret key used for FE/BE."
+  recovery_window_in_days = 7
+}
+
+resource "aws_secretsmanager_secret_version" "api_secret_key" {
+  secret_id     = aws_secretsmanager_secret.api_secret_key.id
+  secret_string = var.api_secret_key
+}
