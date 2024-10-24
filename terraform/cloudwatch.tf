99c1a4543e85bf1a06c6630745a33bbdfe1c5581
domjgiordano <dominick.giordano@payzer.com>
2024-10-24 10:18:49 -0400

    Add chron job for wrapped feature

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -12,3 +12,16 @@ resource "aws_cloudwatch_log_group" "api_log_group" {
     retention_in_days = 14
     tags = merge(local.standard_tags, tomap({ "name"= "${var.app_name}-APIGW-Access-Logs"}))
 }
+
+## CHRON JOB - WRAPPED
+resource "aws_cloudwatch_event_rule" "wrapped_schedule" {
+  name        ="${var.app_name}-wrapped-schedule"
+  description = "Trigger Wrapped Lambda function on the first day of every month"
+  schedule_expression = "cron(0 0 1 * ? *)"  # Runs at midnight on the first day of every month
+}
+
+resource "aws_cloudwatch_event_target" "wrapped_target" {
+  rule      = aws_cloudwatch_event_rule.wrapped_schedule.name
+  target_id = "${var.app_name}-wrapped-target-id"
+  arn       = aws_lambda_function.wrapped.arn
+}
