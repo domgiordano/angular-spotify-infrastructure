c385c0e2017c00ea04e6b80f206bef53ab83e590
domjgiordano <dominick.giordano@payzer.com>
2025-02-20 11:58:18 -0500

    tear down

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -6,12 +6,12 @@ resource "aws_cloudwatch_log_group" "wrapped_db_log_group" {
     tags = merge(local.standard_tags, tomap({ "name"= "${var.app_name}-wrapped"}))
 }
 
-## APIGW
-resource "aws_cloudwatch_log_group" "api_log_group" {
-    name = aws_api_gateway_rest_api.api_gateway.id
-    retention_in_days = 14
-    tags = merge(local.standard_tags, tomap({ "name"= "${var.app_name}-APIGW-Access-Logs"}))
-}
+# ## APIGW
+# resource "aws_cloudwatch_log_group" "api_log_group" {
+#     name = aws_api_gateway_rest_api.api_gateway.id
+#     retention_in_days = 14
+#     tags = merge(local.standard_tags, tomap({ "name"= "${var.app_name}-APIGW-Access-Logs"}))
+# }
 
 ## CHRON JOB - WRAPPED
 resource "aws_cloudwatch_event_rule" "wrapped_schedule" {
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,155 +1,155 @@
-## VPC Resources
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+# ## VPC Resources
+# resource "aws_vpc" "vpc" {
+#   cidr_block = "10.0.0.0/16"
+#   enable_dns_support = true
+#   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${var.app_name}-vpc"
-  }
-}
+#   tags = {
+#     Name = "${var.app_name}-vpc"
+#   }
+# }
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"  # Change as needed
+# resource "aws_subnet" "public_subnet_1" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = "us-east-1a"  # Change as needed
 
-  tags = {
-    Name = "PublicSubnet1"
-  }
-}
+#   tags = {
+#     Name = "PublicSubnet1"
+#   }
+# }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"  # Change as needed
+# resource "aws_subnet" "public_subnet_2" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.2.0/24"
+#   availability_zone = "us-east-1b"  # Change as needed
 
-  tags = {
-    Name = "PublicSubnet2"
-  }
-}
+#   tags = {
+#     Name = "PublicSubnet2"
+#   }
+# }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1c"  # Change as needed
+# resource "aws_subnet" "private_subnet_1" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.3.0/24"
+#   availability_zone = "us-east-1c"  # Change as needed
 
-  tags = {
-    Name = "PrivateSubnet1"
-  }
-}
+#   tags = {
+#     Name = "PrivateSubnet1"
+#   }
+# }
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1d"  # Change as needed
+# resource "aws_subnet" "private_subnet_2" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.4.0/24"
+#   availability_zone = "us-east-1d"  # Change as needed
 
-  tags = {
-    Name = "PrivateSubnet2"
-  }
-}
+#   tags = {
+#     Name = "PrivateSubnet2"
+#   }
+# }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name = "${var.app_name}-igw"
-  }
-}
+#   tags = {
+#     Name = "${var.app_name}-igw"
+#   }
+# }
 
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "public_route_table" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
 
-  tags = {
-    Name = "${var.app_name}-pub-rt"
-  }
-}
+#   tags = {
+#     Name = "${var.app_name}-pub-rt"
+#   }
+# }
 
-resource "aws_route_table_association" "public_subnet_1_association" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_route_table.id
-}
+# resource "aws_route_table_association" "public_subnet_1_association" {
+#   subnet_id      = aws_subnet.public_subnet_1.id
+#   route_table_id = aws_route_table.public_route_table.id
+# }
 
-resource "aws_route_table_association" "public_subnet_2_association" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_route_table.id
-}
+# resource "aws_route_table_association" "public_subnet_2_association" {
+#   subnet_id      = aws_subnet.public_subnet_2.id
+#   route_table_id = aws_route_table.public_route_table.id
+# }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id    = aws_subnet.public_subnet_1.id
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id    = aws_subnet.public_subnet_1.id
 
-  tags = {
-    Name = "${var.app_name}-ngw"
-  }
-}
+#   tags = {
+#     Name = "${var.app_name}-ngw"
+#   }
+# }
 
-resource "aws_eip" "nat_eip" {
-  tags = {
-    Name = "${var.app_name}-nat-eip"
-  }
-}
+# resource "aws_eip" "nat_eip" {
+#   tags = {
+#     Name = "${var.app_name}-nat-eip"
+#   }
+# }
 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private_route_table" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat.id
+#   }
 
-  tags = {
-    Name = "${var.app_name}-priv-rt"
-  }
-}
+#   tags = {
+#     Name = "${var.app_name}-priv-rt"
+#   }
+# }
 
-resource "aws_route_table_association" "private_subnet_1_association" {
-  subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private_route_table.id
-}
+# resource "aws_route_table_association" "private_subnet_1_association" {
+#   subnet_id      = aws_subnet.private_subnet_1.id
+#   route_table_id = aws_route_table.private_route_table.id
+# }
 
-resource "aws_route_table_association" "private_subnet_2_association" {
-  subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private_route_table.id
-}
+# resource "aws_route_table_association" "private_subnet_2_association" {
+#   subnet_id      = aws_subnet.private_subnet_2.id
+#   route_table_id = aws_route_table.private_route_table.id
+# }
 
-resource "aws_security_group" "lambda_sg" {
-  name   = "${var.app_name}-lambda-sg"
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_security_group" "lambda_sg" {
+#   name   = "${var.app_name}-lambda-sg"
+#   vpc_id = aws_vpc.vpc.id
 
-#   ingress {
+# #   ingress {
+# #     from_port   = 0
+# #     to_port     = 65535
+# #     protocol    = "tcp"
+# #     cidr_blocks = ["0.0.0.0/0"]
+# #   }
+#     ingress {
+#         from_port       = 0
+#         to_port         = 65535
+#         protocol        = "tcp"
+#         cidr_blocks     = ["10.0.0.0/16"]  # Replace with your VPC CIDR block
+#     }
+#     ingress {
+#         from_port       = 0
+#         to_port         = 65535
+#         protocol        = "tcp"
+#         self            = true  # Allow traffic from the same security group
+#     }
+
+#   egress {
 #     from_port   = 0
-#     to_port     = 65535
-#     protocol    = "tcp"
+#     to_port     = 0
+#     protocol    = "-1"  # All traffic
 #     cidr_blocks = ["0.0.0.0/0"]
 #   }
-    ingress {
-        from_port       = 0
-        to_port         = 65535
-        protocol        = "tcp"
-        cidr_blocks     = ["10.0.0.0/16"]  # Replace with your VPC CIDR block
-    }
-    ingress {
-        from_port       = 0
-        to_port         = 65535
-        protocol        = "tcp"
-        self            = true  # Allow traffic from the same security group
-    }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # All traffic
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "LambdaSecurityGroup"
-  }
-}
+#   tags = {
+#     Name = "LambdaSecurityGroup"
+#   }
+# }
 
