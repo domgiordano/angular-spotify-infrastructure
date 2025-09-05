e58902da2b723640f10faa8215df705854809d8c
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2025-09-05 08:37:25 -0400

    Add new lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -103,3 +103,48 @@ resource "aws_lambda_permission" "release_radar" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.release_radar_schedule.arn
 }
+
+resource "aws_lambda_function" "update_user_table" {
+
+  function_name     = "${var.app_name}-update-user-table"
+  filename          = "./templates/lambda_stub.zip"
+  source_code_hash  = filebase64sha256("./templates/lambda_stub.zip")
+  handler           = "handler.handler"
+  layers            = [aws_lambda_layer_version.lambda_layer.arn]
+  runtime           = var.lambda_runtime
+  memory_size       = 1024
+  timeout           = 900
+  role              = aws_iam_role.lambda_role.arn
+  environment {
+    variables = local.lambda_variables
+  }
+
+  # vpc_config {
+  #   subnet_ids         = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  #   security_group_ids = [aws_security_group.lambda_sg.id]
+  # }
+
+  tags = merge(local.standard_tags, tomap({"name" = "${var.app_name}-update-user-table"}))
+
+
+
+  tracing_config {
+    mode = var.lambda_trace_mode
+  }
+
+
+
+  lifecycle {
+    ignore_changes = [
+      description,
+      filename,
+      source_code_hash,
+      layers
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy.lambda_role_policy,
+    aws_iam_role.lambda_role
+  ]
+}
